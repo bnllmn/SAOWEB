29c0e137ff7175ce7e10534a370b68e9fee12c27
bnllmn <31971205+bnllmn@users.noreply.github.com>
2022-07-24 19:45:26 -0400

    initial styling and eslint+prettier config

---
+++ src/pages/DownloadPage.tsx
@@ -0,0 +1,5 @@
+function DownloadPage() {
+  return <div className="h-full w-4/5 bg-white mx-auto bg-bg2"></div>;
+}
+
+export default DownloadPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#1e1e1f',
+        bg2: '#323232'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
