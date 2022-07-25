2e0222f1c4f54dff0be002d61d026e0c9149c887
bnllmn <31971205+bnllmn@users.noreply.github.com>
2022-07-25 15:00:26 -0400

    Revert "Updates"
    This reverts commit 470ee975f01215c9fd56f65e3aacc10b4f3d6413.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#1e1e1f',
+        bg2: '#323232'
+      }
+    }
+  },
+  plugins: []
+};
