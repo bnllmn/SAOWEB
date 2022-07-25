470ee975f01215c9fd56f65e3aacc10b4f3d6413
bnllmn <31971205+bnllmn@users.noreply.github.com>
2022-07-25 14:41:20 -0400

    Updates

--- src/components/PageContentContainer/PageContentContainer.tsx
+++
@@ -1,5 +0,0 @@
-type Props = { children?: JSX.Element | JSX.Element[] };
-
-export const PageContentContainer: React.FC<Props> = ({ children }) => (
-  <div className="h-full w-4/5 bg-white mx-auto bg-bg2">{children}</div>
-);
--- src/components/UpdatePosts/components/UpdatePostLandingSample/UpdatePostLandingSummary.tsx
+++
@@ -1,9 +0,0 @@
-import { UpdatePostData } from '../../posts/updateposts';
-
-type UpdatePostBodyProps = {
-  postData: UpdatePostData;
-};
-
-export const UpdatePostLandingSummary = (props: UpdatePostBodyProps) => {
-  return <div className="p-2">{props.postData.landingsummary}</div>;
-};
